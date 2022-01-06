216646d83af6b0d2719295bcac463c4a3df53bd8
emamagic <developermagic7@gmail.com>
2022-01-06 13:15:59 +0330

    project name changed

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -2,6 +2,6 @@ object Android {
     const val minSdk = 21
     const val targetSdk = 31
     const val compileSdk = 31
-    const val applicationId = "com.emamagic.limonad_android"
-    const val projectName = "Limonad_Android"
+    const val applicationId = "com.emamagic.android_multi_modules"
+    const val projectName = "Android-Multi-Module"
 }
