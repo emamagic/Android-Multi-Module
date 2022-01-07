61c15142760d5df5d9ca514da97cb02eab30be8e
emamagic <developermagic7@gmail.com>
2022-01-07 10:22:42 +0330

    read me added

--- data/network/src/main/java/com/emamagic/network/util/Const.kt
+++ data/network/src/main/java/com/emamagic/network/util/Const.kt
@@ -2,8 +2,6 @@ package com.emamagic.network.util
 
 object Const {
 
-    //    const val BASE_URL = "https://test"
-    const val BASE_URL = "https://alpha.limonadapp.ir/Limonad/"
-    const val API_KEY = "8731d3f878b0495fbe92aa00f826ca42"
+        const val BASE_URL = "https://test"
 
 }
